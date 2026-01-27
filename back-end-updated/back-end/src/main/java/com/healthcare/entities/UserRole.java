134926fa4ba333bf05fd5443484f3c0a948a17ab
Ketan Aher <141157327+ketanaher@users.noreply.github.com>
2026-01-27 21:50:52 +0530

    Delete back-end-updated/back-end directory

--- back-end-updated/back-end/src/main/java/com/healthcare/entities/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.healthcare.entities;
-
-public enum UserRole {
-	ROLE_ADMIN, ROLE_PATIENT, ROLE_DOCTOR
-}
--- back-end-updated/back-end/src/main/java/com/healthcare/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.healthcare.service;
-
-import com.healthcare.dtos.LoginRequest;
-import com.healthcare.dtos.LoginResponse;
-
-public interface AuthService {
-    LoginResponse login(LoginRequest request);
-}
